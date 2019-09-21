54b4092026d783d7c70e6daaf564b8e750433cb6
conditmatt <matthew@humanyze.com>
2019-09-21 17:34:56 -0400

    add it all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Router, Link } from '@reach/router';
+
+const Big = React.lazy(() => import('./big'));
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hey fart boi 
       </header>
+      <Link to='/big'>big boi</Link>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <Big path='/big' />
+        </Router>
+      </React.Suspense>
     </div>
   );
 }
